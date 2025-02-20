346618643a5a2e1c81f982043a928a650950244a
wilmarcartwright <udaykumarreddy27@gmail.com>
2025-02-20 23:27:20 +0530

    Normalize line endings

--- aks.tf
+++ aks.tf
@@ -1,1 +1,33 @@
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  dns_prefix          = "aks-dns"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_D2_v2"
+    vnet_subnet_id = azurerm_subnet.subnet1
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
 
