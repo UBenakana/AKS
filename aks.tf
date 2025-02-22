ba70326dd14f9e80b3bd4d2741a64aaaa89223dd
wilmarcartwright <udaykumarreddy27@gmail.com>
2025-02-23 00:37:33 +0530

    updated the terraform script 23/02/2025

--- aks.tf
+++ aks.tf
@@ -1,39 +1,40 @@
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster
   location            = azurerm_resource_group.resourcegroup.location
   resource_group_name = azurerm_resource_group.resourcegroup.name
-  dns_prefix          = "aks-dns"
+  dns_prefix          = "exampleaks"
+
+  linux_profile {
+    admin_username = "ubuntu"
+    ssh_key {
+      key_data = file("C:\\Users\\udayk\\.ssh\\id_rsa.pub")
+    }
+  }
 
   default_node_pool {
     name           = "default"
     node_count     = 1
-    vm_size        = "Standard_D2_v2"
+    vm_size        = "Standard_B2ms"
     vnet_subnet_id = azurerm_subnet.subnet1.id
-  }
-
-  network_profile {
-    network_plugin = "azure"
-    service_cidr   = "10.1.0.0/16"
-    dns_service_ip = "10.1.0.10"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    Environment = "Production"
+  network_profile {
+    network_plugin = "kubenet"
+    dns_service_ip = "10.0.0.10"
+    service_cidr   = "10.0.0.0/16"
   }
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
-
--- terraform.tfvars
+++ terraform.tfvars
@@ -4,7 +4,7 @@
 resource_group_name   = "my-aks-resource-group"
 region                = "eastus"
 vnet_name             = "aks-vnet"
-address_space         = ["10.0.0.0/16"]
+address_space         = ["10.200.0.0/16"]
 subnet1_name          = "aks-subnet1"
-subnet_address_prefix = ["10.0.1.0/24"]
-aks_cluster           = "dev-aks-cluster"
+subnet_address_prefix = ["10.200.1.0/24"]
+aks_cluster           = "aks-cluster"
